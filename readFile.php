19d1d9a6aa849b23fbe34d87371ee0f00ab6cbca
vijay <vijay@admarvel.com>
2015-03-10 23:26:04 +0530

    initial commit

---
+++ readFile.php
@@ -0,0 +1,20 @@
+<?php
+
+// read contents from the input stream
+$inputHandler = fopen('php://input', "r");
+// create a temp file where to save data from the input stream
+$fileHandler = fopen('./myfile.pdf', "w+");
+
+// save data from the input stream
+while(true) {
+	$buffer = fgets($inputHandler, 4096);
+	if (strlen($buffer) == 0) {
+		fclose($inputHandler);
+		fclose($fileHandler);
+		return true;
+	}
+
+	fwrite($fileHandler, $buffer);
+}
+// done
+?>
